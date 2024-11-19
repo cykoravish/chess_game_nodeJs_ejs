16298da66b2f06bb7cedc3b07ad705566701b09b
Ravish Bisht <ravishbisht85.com>
2024-11-19 19:52:42 +0530

    chess logic  write

--- app.js
+++ app.js
@@ -21,8 +21,43 @@ app.get("/", (req, res) => {
 
 io.on("connection", function (uniquesocket) {
   console.log("connected");
-  uniquesocket.on("baby", () => {
-    io.emit("baby mil gya");
+
+  if (!players.white) {
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerRole", "b");
+  } else {
+    uniquesocket.emit("spectatorRole");
+  }
+
+  uniquesocket.on("disconnect", function () {
+    if (uniquesocket.id === players.white) {
+      delete players.white;
+    } else if (uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniquesocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniquesocket.id !== players.white) return;
+      if (chess.turn() === "b" && uniquesocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("invalid move", move);
+        uniquesocket.emit("invalidMove", move);
+      }
+    } catch (error) {
+      console.log("error: ", err);
+      uniquesocket.emit("Invalid move", move);
+    }
   });
 });
 
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,7 +1,2 @@
 const socket = io();
 
-socket.emit("baby");
-socket.on("baby mil gya",()=>{
-    console.log("ok res agay wapiss")
-});
-
