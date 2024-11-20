c29279af824e364a8517f058f9e5267a7a4a4b4c
Ravish Bisht <ravishbisht85.com>
2024-11-20 07:53:15 +0530

    chess ui and js logic write

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,2 +1,52 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
 
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {
+  const board = chess.board();
+  boardElement.innerHTML = "";
+  board.forEach((row, rowindex) => {
+    row.forEach((square, squareindex) => {
+      const squareElement = document.createElement("div");
+      squareElement.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark"
+      );
+      squareElement.dataset.row = rowindex;
+      squareElement.dataset.col = squareindex;
+
+      if (square) {
+        const pieceElement = document.createElement("div");
+        pieceElement.classList.add(
+          "piece",
+          square.color === "w" ? "white" : "black"
+        );
+      }
+      pieceElement.innerText = "";
+      pieceElement.draggable = playerRole === square.color;
+
+      pieceElement.addEventListener("dragstart", () => {
+        if (pieceElement.draggable) {
+          draggedPiece = pieceElement;
+          sourceSquare = { row: rowindex, col: squareindex };
+          e.dataTransfer.setData("text/plain", "");
+        }
+      });
+      pieceElement.addEventListener("dragend", (e) => {
+        draggedPiece = null;
+        sourceSquare = null;
+      });
+      squareElement.appendChild(pieceElement)
+    });
+  });
+};
+
+const handleMove = () => {};
+
+const getPiecesUnicode = () => {};
+
+renderBoard();
