981bd2a430b6307c09ccf622d9836b9f69723284
Ravish Bisht <ravishbisht85.com>
2024-11-20 08:48:24 +0530

    remder players in board

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -25,28 +25,59 @@ const renderBoard = () => {
           "piece",
           square.color === "w" ? "white" : "black"
         );
-      }
-      pieceElement.innerText = "";
-      pieceElement.draggable = playerRole === square.color;
+        pieceElement.innerText = getPiecesUnicode(square);
+        pieceElement.draggable = playerRole === square.color;
 
-      pieceElement.addEventListener("dragstart", () => {
-        if (pieceElement.draggable) {
-          draggedPiece = pieceElement;
-          sourceSquare = { row: rowindex, col: squareindex };
-          e.dataTransfer.setData("text/plain", "");
+        pieceElement.addEventListener("dragstart", () => {
+          if (pieceElement.draggable) {
+            draggedPiece = pieceElement;
+            sourceSquare = { row: rowindex, col: squareindex };
+            e.dataTransfer.setData("text/plain", "");
+          }
+        });
+        pieceElement.addEventListener("dragend", (e) => {
+          draggedPiece = null;
+          sourceSquare = null;
+        });
+        squareElement.appendChild(pieceElement);
+      }
+
+      squareElement.addEventListener("dragover", function (e) {
+        e.preventDefault();
+      });
+      squareElement.addEventListener("drop", function (e) {
+        e.preventDefault();
+        if (draggedPiece) {
+          const targetSource = {
+            row: parseInt(squareElement.dataset.row),
+            col: parseInt(squareElement.dataset.col),
+          };
+          handleMove(sourceSquare, targetSource);
         }
       });
-      pieceElement.addEventListener("dragend", (e) => {
-        draggedPiece = null;
-        sourceSquare = null;
-      });
-      squareElement.appendChild(pieceElement)
+      boardElement.appendChild(squareElement);
     });
   });
 };
 
 const handleMove = () => {};
 
-const getPiecesUnicode = () => {};
+const getPiecesUnicode = (piece) => {
+  const unicodePieces = {
+    p: "♟",
+    r: "♜",
+    n: "♞",
+    b: "♝",
+    q: "♛",
+    k: "♚",
+    P: "♙",
+    R: "♖",
+    N: "♘",
+    B: "♗",
+    Q: "♕",
+    K: "♔",
+  };
+  return unicodePieces[piece.type] || "";
+};
 
 renderBoard();
